d412785b127fd63a2d99a878d52150dce480611c
colby <colbykrawczyk10@gmail.com>
2022-04-10 01:24:48 -0400

    Added a settings manager

--- src/main/java/net/elytrapvp/elytrastaff/ElytraStaff.java
+++ src/main/java/net/elytrapvp/elytrastaff/ElytraStaff.java
@@ -4,12 +4,24 @@ import net.elytrapvp.elytrastaff.commands.AbstractCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class ElytraStaff extends JavaPlugin {
+    private SettingsManager settingsManager;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
 
+        // Load plugin settings.
+        settingsManager = new SettingsManager(this);
+
         // Register our commands with Spigot.
         AbstractCommand.registerCommands(this);
+    }
+
+    /**
+     * Get the settings manager.
+     * @return Settings Manager.
+     */
+    public SettingsManager getSettingsManager() {
+        return settingsManager;
     }
 }
